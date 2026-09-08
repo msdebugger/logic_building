ed21ef2e5b972b60162de8f1e2ac34335b46d9b7
msdebugger <thisiservarshak@gmail.com>
2026-09-08 22:43:39 +0530

    Variables and Operators

---
+++ 1 and 2 Variables and Operators/05_number_input.py
@@ -0,0 +1,3 @@
+number = int(input("Enter number: "))
+
+print(f"Value of number after power {number**3} and Value after number // by 7 is {number // 7}, Value after number / by 7 is {number / 7}, also value after number % by 7 is {number % 7}")
---
+++ 1 and 2 Variables and Operators/06_twonumber_product.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+result = 0
+
+for i in range(num2):
+    result += num1
+
+print(f"Result is: {result}")    
