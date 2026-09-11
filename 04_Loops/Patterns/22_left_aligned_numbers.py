eb8e4aa29f6636b2b973b342ed88359e04ec3668
msdebugger <thisiservarshak@gmail.com>
2026-09-11 17:30:20 +0530

    Patter Part1

---
+++ 04_Loops/Patterns/22_left_aligned_numbers.py
@@ -0,0 +1,14 @@
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+num = int(input("Enter number: "))
+
+for i in range(1, num+1):
+    for j in range(1, i+1):
+        print(f"{j}", end=" ")
+    print()
