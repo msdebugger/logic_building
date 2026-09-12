7c8fdb4663c938b484da2160a04ec8424579c3e7
msdebugger <thisiservarshak@gmail.com>
2026-09-12 21:46:12 +0530

    23_numbered

---
+++ 04_Loops/Patterns/23_left_aligned_number_dec.py
@@ -0,0 +1,15 @@
+"""
+23: Write a nested loop program to print the following pattern:
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+"""
+
+num = int(input("Enter number: "))
+
+for i in range (1, num+1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()    
