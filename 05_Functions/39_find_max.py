11d566e08510f126d7a566f821126e98d15ba898
msdebugger <thisiservarshak@gmail.com>
2026-09-23 22:55:05 +0530

    Function Part3

---
+++ 05_Functions/39_find_max.py
@@ -0,0 +1,13 @@
+"""
+Q39: Write a function called find_max that takes three numbers as parameters and prints the largest one.
+"""
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+def find_max(a, b, c):
+    print(max(num1, num2, num3))
+
+find_max(num1, num2, num3)    
+
