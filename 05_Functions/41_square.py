ee0b2661d815890faef5340b683eb060f5d2ab20
msdebugger <thisiservarshak@gmail.com>
2026-09-25 22:22:52 +0530

    Function Part5

---
+++ 05_Functions/41_square.py
@@ -0,0 +1,10 @@
+"""
+Q41: Write a function called square that takes a number and returns its square. Store the result and print it.
+"""
+
+num = int(input("Enter number: "))
+
+def square(num):
+    return num*num
+
+print(square(num))
